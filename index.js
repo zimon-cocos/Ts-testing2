83168e1bac1e83f0fa5e22de03a78061693d3bbd
zimon-cocos <111566187+zimon-cocos@users.noreply.github.com>
2025-04-25 19:44:35 +0200

    slusny navrh showcase obrazkov, funkcny toggle

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ var number = 1;
 if (svgCircle && sect2 && clickShapeBox) {
     svgCircle.addEventListener("click", function () {
         console.log("Clicked circle svg");
-        if (sect2.style.maxHeight) {
+        if (sect2.offsetHeight > 10) {
             sect2.style.maxHeight = "0";
+            console.log("collapsing");
         }
         else {
             sect2.style.maxHeight = sect2.scrollHeight + "px";
+            console.log("expanding");
         }
         console.log("sect2 height is", sect2.offsetHeight);
     });
--- index.ts
+++ index.ts
@@ -11,17 +11,20 @@ var number: number = 1
 
 if(svgCircle && sect2 && clickShapeBox)
 {
+
     svgCircle.addEventListener("click",() =>
     {
+
         console.log("Clicked circle svg")
-        if(sect2.style.maxHeight)
+        if(sect2.offsetHeight > 10)
         {
             sect2.style.maxHeight = "0"
+            console.log("collapsing")
         }
         else
         {
             sect2.style.maxHeight = sect2.scrollHeight + "px";
-
+            console.log("expanding")
         }
         console.log("sect2 height is",sect2.offsetHeight)
 
