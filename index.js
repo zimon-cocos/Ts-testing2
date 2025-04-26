a6051499fbc405d059adec7f528bc59adbca08c2
zimon-cocos <111566187+zimon-cocos@users.noreply.github.com>
2025-04-26 10:56:46 +0200

    lepsi showcase rollout

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var svgCircle = document.getElementById("svgCircle");
 var clickShapeBox = document.getElementById("clickShapeBox");
 var sect2 = document.getElementById("sect2");
-var number = 1;
+var arrow = document.getElementById("arrow");
+var pointUp = false;
+var path = document.getElementById("arrowPath");
 if (svgCircle && sect2 && clickShapeBox) {
     svgCircle.addEventListener("click", function () {
         console.log("Clicked circle svg");
@@ -14,5 +16,23 @@ if (svgCircle && sect2 && clickShapeBox) {
             console.log("expanding");
         }
         console.log("sect2 height is", sect2.offsetHeight);
+    });
+}
+if (arrow && path && sect2 && clickShapeBox) {
+    arrow.addEventListener("click", function () {
+        console.log("click");
+        pointUp = !pointUp;
+        if (pointUp) {
+            console.log("was pointing up");
+            path.setAttribute("d", "M 100 50 L150 100 L200 50");
+            sect2.style.maxHeight = "0";
+            console.log("collapsing");
+        }
+        else {
+            console.log("was pointing down");
+            path.setAttribute("d", "M100 50 L150 0 L200 50");
+            sect2.style.maxHeight = sect2.scrollHeight + "px";
+            console.log("expanding");
+        }
     });
 }
--- index.ts
+++ index.ts
@@ -3,9 +3,11 @@ const svgCircle = document.getElementById("svgCircle") as HTMLElement | null;
 const clickShapeBox = document.getElementById("clickShapeBox") as HTMLElement | null;
 const sect2 = document.getElementById("sect2") as HTMLElement | null;
 
+const arrow = document.getElementById("arrow") as HTMLElement | null;
+let pointUp = false;
 
+const path = document.getElementById("arrowPath") as HTMLElement | null;
 
-var number: number = 1
 
 
 
@@ -33,3 +35,29 @@ if(svgCircle && sect2 && clickShapeBox)
     )
 }
 
+if(arrow && path && sect2 && clickShapeBox) 
+    {
+        arrow.addEventListener("click", ()=>
+        {
+            console.log("click")
+            pointUp = !pointUp
+           
+    
+            if(pointUp)
+            {
+                console.log("was pointing up")
+                path.setAttribute("d", "M 100 50 L150 100 L200 50")
+                sect2.style.maxHeight = "0"
+                console.log("collapsing")          
+                
+            }
+            else
+            {
+                console.log("was pointing down")           
+                path.setAttribute("d","M100 50 L150 0 L200 50")
+                sect2.style.maxHeight = sect2.scrollHeight + "px";
+                console.log("expanding")             
+            }
+    
+        })
+    }
